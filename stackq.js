0a1eb072435c9e096189c4759804051c5b9e239c
Alexander Ewetumo <trinoxf@gmail.com>
2015-01-03 22:33:20 +0100

    removing clutter

--- stackq.js
+++ stackq.js
@@ -2,29 +2,6 @@ var as = {};
 require('./lib/as.js')(as);
 require('./lib/as-contrib.js')(as);
 require('./lib/ds.js')(as);
-// require('./lib/streams.js')(as);
 require('./lib/extenders.js')(as);
-// require('./lib/schemes.js')(as);
 
 module.exports = as;
-// var structs = require('./lib/structs.js');
-// var schemes = require('./lib/schemes.js');
-// module.exports = {
-  // 'as': as,
-  // 'ds': as.DS,
-  // 'streams': as.Streams,
-  // 'ascontrib':as,
-  // 'structs': structs,
-  // 'schemes': schemes,
-
-  //useful bits
-  // 'Class': as.funcs.bind(structs.Class,structs),
-  // 'Jazz': as.funcs.bind(structs.JzGroup,structs),
-  // 'Expects': structs.Expects,
-  // 'Utility': as.Util,
-  // 'Enums': as.enums,
-  // 'Valids': as.valids,
-  // 'Funcs': as.funcs,
-  // 'Tags': as.tags,
-  // 'Notify': as.notify
-// }
