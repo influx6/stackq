93cd1301ddbd92881455cfe7fe09c382d09a311b
Alexander Ewetumo <trinoxf@gmail.com>
2015-01-25 22:11:55 +0100

    heapsimplesort done

--- tests/compare.js
+++ tests/compare.js
@@ -1,40 +1,40 @@
-var stacks = require('../stackq');
+var stacks = require('../stackq'),
+    enums = stacks.enums;
 
 stacks.Jazz('compare_engine specifications',function(_){
 
-  var list = [1,3,45,6,7,5,65,2,565,23,45,677,2325,4,665,4545];
+  var list = [300,3,45,6,1,5,65,2,565,23,45,677,2325,4,665,4545];
   var range = { from: 1, to: 7 };
+  var xrange = stacks.Util.extends({},range);
 
-  _('is the largest number == 65 from indexes: '+JSON.stringify(range),function(k){
+  _('is the largest number == 65 from indexes: 1 to 7',function(k){
     k.sync(function(d,g){
-     stacks.Expects.is(stacks.enums.compareEngine(list,function(m,n){
-       return m > n;
-     },stacks.Util.extends({},range)),65);
+     stacks.Expects.is(enums.compareEngine(list,enums.max(),range),65);
     });
   }).use(list);
 
-  _('is the largest number == 2 from indexes: '+JSON.stringify(range),function(k){
+  _('is the min number == 2 from indexes: 1 to 7',function(k){
     k.sync(function(d,g){
-     stacks.Expects.is(stacks.enums.compareEngine(list,function(m,n){
-       return m < n;
-     },stacks.Util.extends({},range)),2);
+     stacks.Expects.is(enums.compareEngine(list,enums.min(),xrange),2);
     });
   }).use(list);
 
   _('is the largest number == 4545',function(k){
     k.sync(function(d,g){
-     stacks.Expects.is(stacks.enums.compareEngine(list,function(m,n){
-       return m > n;
-     }),4545);
+     stacks.Expects.is(enums.compareEngine(list,enums.max()),4545);
     });
   }).use(list);
 
   _('is the smallest number == 1',function(k){
     k.sync(function(d,g){
-     stacks.Expects.is(stacks.enums.compareEngine(list,function(m,n){
-       return m < n;
-     }),1);
+     stacks.Expects.is(enums.compareEngine(list,enums.min()),1);
     });
   }).use(list);
+
+  _('is the sorted list equal to the originals length of '+list.length,function(k){
+    k.sync(function(d,g){
+     stacks.Expects.is(d.length,list.length);
+    });
+  }).use(enums.heapSortSimple(list));
 
 });
