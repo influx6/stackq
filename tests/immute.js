b30e6bce5c64553ccde0405e4dd6a3073668a788
Alexander Ewetumo <trinoxf@gmail.com>
2015-01-27 17:46:43 +0100

    first stable mutator

--- tests/immute.js
+++ tests/immute.js
@@ -1,24 +1,58 @@
 var _ = require('../stackq');
 _.Jazz('Immutate specifications',function(k){
 
-  var val = _.Immutate.value('alex');
-  var vad = _.Immutate.value({ a:1, b:2, c: 5});
-  var vax = _.Immutate.value(vad);
-  var vaxd = vax.set('b',40);
+  var single = _.Immutate.value('alex');
 
-  k('can i create a immutate',function($){
+  k('can i create a single value immutate',function($){
     $.sync(function(m){
-      _.Expects.truthy(m);
-      _.Expects.isTrue(_.Immutate.isType(m));
-      _.Expects.isTrue(_.Immutate.isInstance(m));
+      _.Expects.isTrue(_.Immutate.instanceBelongs(m));
+      _.Expects.isObject(m.toObject());
+      _.Expects.is(_.enums.pluckIn(m.toObject(),['value']),'alex');
+      _.Expects.isTrue(m.has('value'));
     });
-  }).use(val);
+  }).use(single);
 
+  var map = { a:1, b: 2, c: { e: 3 } };
+  var atom = _.Immutate.value(map);
 
-  val.then(_.tags.tagDefer('val-ready'));
-  vad.then(_.tags.tagDefer('vad-ready'));
-  vax.then(_.tags.tagDefer('vax-ready'));
+  k('can i create a object immutate',function($){
+    $.sync(function(m){
+      _.Expects.isTrue(_.Immutate.instanceBelongs(m));
+      _.Expects.is(m.toObject(),map);
+    });
+  }).use(atom);
 
-  console.log(_.Future.isType(val));
+  var acs = atom.get('c.e');
+
+  k('can i create a immutate cursor',function($){
+    $.sync(function(m){
+      _.Expects.isTrue(_.ImmutateCursor.instanceBelongs(m));
+      _.Expects.is(m.value(),3);
+    });
+  }).use(acs);
+
+  var cmap = atom.get('c');
+
+  k('is atom.toJS a real clone?',function($){
+    $.sync(function(m){
+      var js = m.toJS(), ob = m.toObject();
+      _.Expects.isNot(ob,js);
+      _.Expects.is(Object.keys(js).length,Object.keys(ob).length);
+    });
+  }).use(atom);
   
+  k('can i get mutate value using cursors',function($){
+    $.sync(function(m){
+      _.Expects.is(m.e,3);
+    });
+  }).use(cmap.value());
+
+  cmap.set('e',20);
+
+  k('can i mutate value using cursors',function($){
+    $.sync(function(m){
+      _.Expects.is(m.e,20);
+    });
+  }).use(cmap.value());
+
 });
