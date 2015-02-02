b99df941a5b15e21c33a28a495813b761eb1baf9
Alexander Ewetumo <trinoxf@gmail.com>
2015-02-02 09:35:04 +0100

    new skipping fix

--- tests/iterator.js
+++ tests/iterator.js
@@ -1,5 +1,5 @@
 var s = require('stackq');
-var f = s.SkipBackwardCollectionIterator([1,3,4,5,6,8,9,20,4,6],4);
+var f = s.SkipForwardCollectionIterator([1,3,4,5,6,8,9,20,4,6],5);
 
 while(f.hasNext()){
   f.moveNext();
