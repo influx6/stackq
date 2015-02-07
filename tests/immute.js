922299d738e5313e108cc8a46cb839030d2a14a6
Alexander Ewetumo <trinoxf@gmail.com>
2015-02-07 18:56:56 +0100

    first stable immutate cursors with ghost curor

--- tests/immute.js
+++ tests/immute.js
@@ -5,65 +5,57 @@ _.Jazz('Immutate specifications',function(k){
   var atom = _.Immutate.transform({a:1,b: 2,c: { e: 3 }});
   var risk = _.Immutate.transform([1,3,4,5,6]);
 
-  var ac = atom.get();
-  var cmap = atom.get('c');
-  var acs = cmap.get('c.e');
-  var kc = key.get();
+  var ac = atom.snapshot();
+  var cmap = atom.snapshot('c');
+  var acs = atom.snapshot('c.e');
+  var kc = key.snapshot();
 
-  kc.newSequence(function(x){
-    return x * 2;
-  });
+  var acg = atom.ghost('c');
 
-  console.log(cmap,acs);
+  k('can i create a object immutate',function($){
+    $.sync(function(m){
+      _.Expects.isTrue(_.Immutate.instanceBelongs(m));
+      _.Expects.is(m.toJS()['a'],1);
+    });
+  }).use(atom);
 
+  k('can i create a immutate cursor',function($){
+    $.sync(function(m){
+      _.Expects.isTrue(_.ImmutateCursor.instanceBelongs(m));
+      _.Expects.isTrue(_.ValueCursor.instanceBelongs(m));
+      _.Expects.is(m.value(),1);
+    });
+  }).use(kc);
 
-  // k('can i create a object immutate',function($){
-  //   $.sync(function(m){
-  //     _.Expects.isTrue(_.Immutate.instanceBelongs(m));
-  //     _.Expects.is(m.toObject()['a'],1);
-  //   });
-  // }).use(atom);
+  k('is atom.toJS a real clone?',function($){
+    $.sync(function(m){
+      var js = m.toJS(), ob = m.value();
+      _.Expects.isNot(ob,js);
+      _.Expects.is(Object.keys(js).length,Object.keys(ob).length);
+    });
+  }).use(atom);
+  
+  k('can i get mutate value using cursors',function($){
+    $.sync(function(m){
+      _.Expects.is(m,3);
+    });
+  }).use(acs.value());
 
-  // k('can i create a immutate cursor',function($){
-  //   $.sync(function(m){
-  //     _.Expects.isTrue(_.ImmutateCursor.instanceBelongs(m));
-  //     _.Expects.isTrue(_.ValueCursor.instanceBelongs(m));
-  //     _.Expects.is(m.value(),1);
-  //   });
-  // }).use(key.get());
+  acs.set(20);
 
-
-  // k('is atom.toJS a real clone?',function($){
-  //   $.sync(function(m){
-  //     var js = m.toJS(), ob = m.toObject();
-  //     _.Expects.isNot(ob,js);
-  //     _.Expects.is(Object.keys(js).length,Object.keys(ob).length);
-  //   });
-  // }).use(atom);
-  // 
-  // k('can i get mutate value using cursors',function($){
-  //   $.sync(function(m){
-  //     _.Expects.is(m,3);
-  //   });
-  // }).use(acs.value());
-
-  // acs.set(20);
-
-  // k('can i mutate value using cursors',function($){
-  //   $.sync(function(m){
-  //     _.Expects.is(m,20);
-  //   });
-  // }).use(acs.value());
+  k('can i mutate value using cursors',function($){
+    $.sync(function(m){
+      _.Expects.is(m,20);
+    });
+  }).use(acs.value());
  
-  // cmap.set('b','rocker');
+  cmap.set('b','rocker');
 
-  // k('can i add value using cursors',function($){
-  //   $.sync(function(m){
-  //     _.Expects.is(m,'rocker');
-  //   });
-  // }).use(cmap.get('b').value());
+  k('can i add value using cursors',function($){
+    $.sync(function(m){
+      _.Expects.is(m,'rocker');
+    });
+  }).use(cmap.get('b').value());
 
-  // cmap.set('f',risk);
-  // console.log('rk:',cmap.toJS());
 
 });
